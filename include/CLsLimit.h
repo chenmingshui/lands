20a2cd6e637ca71d2e3f1fbf526024c818437f16
mschen <>
2012-02-15 15:55:53 +0000

    for dumping info on the inputNuisaneSigma, and change RooDataSet to RooAbsData for generization

--- include/CLsLimit.h
+++ include/CLsLimit.h
@@ -19,6 +19,7 @@ namespace lands{
 	extern TF1* fitToRvsCL_expo;
 	extern double _significances;
 	extern double *_inputNuisances;
+	extern double *_inputNuisancesSigma;
 	extern double *_startNuisances; // _xxxxNuisances[0] is the POI signal strength
 	extern double *_minNuisances;
 	extern double *_maxNuisances;
@@ -33,7 +34,7 @@ namespace lands{
 	extern bool _bDumpFinalFitResults;
 	void Chisquare(Int_t &npar, Double_t *gin, Double_t &f,  Double_t *par, Int_t iflag); 
 	double MinuitFit(int model, double &r, double &er, double mu=0, double *pars=0, bool hasBestFitted = false, int debug=0, int *success=0);
-	bool DoAfit(double mu, vector<double> vdata, vector<RooDataSet*> vrds, double* pars);
+	bool DoAfit(double mu, vector<double> vdata, vector<RooAbsData*> vrds, double* pars);
 	class CLsBase
 	{
 		public:
